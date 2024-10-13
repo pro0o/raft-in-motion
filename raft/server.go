91077febacb2aa6837aa03808dc31d076b99220a
pro0o <pp48041721@student.ku.edu.np>
2024-10-13 16:52:15 +0545

    chore(server): skeleton for RPC.

---
+++ raft/server.go
@@ -0,0 +1,167 @@
+package raft
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+
+type Server struct {
+	mu sync.Mutex
+
+	serverId int
+	peerIds  []int
+
+	rf       *Raft
+	rpcProxy *RPCProxy
+
+	rpcServer *rpc.Server
+	listener  net.Listener
+
+	peerClients map[int]*rpc.Client
+
+	ready <-chan any
+	quit  chan any
+	wg    sync.WaitGroup
+}
+
+type RPCProxy struct {
+	rf *Raft
+}
+
+func NewServer(serverId int, peerIds []int, ready <-chan any) *Server {
+	s := new(Server)
+	s.serverId = serverId
+	s.peerIds = peerIds
+	s.peerClients = make(map[int]*rpc.Client)
+	s.ready = ready
+	s.quit = make(chan any)
+	return s
+}
+
+func (s *Server) Serve() {
+	s.mu.Lock()
+	s.rf = Make(s.serverId, s.peerIds, s, s.ready)
+
+	// Create a new RPC server and register a RPCProxy that forwards all methods
+	// to n.rf
+	s.rpcServer = rpc.NewServer()
+	s.rpcProxy = &RPCProxy{rf: s.rf}
+	s.rpcServer.RegisterName("Raft", s.rpcProxy)
+
+	var err error
+	s.listener, err = net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("[%v] listening at %s", s.serverId, s.listener.Addr())
+	s.mu.Unlock()
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+
+		for {
+			conn, err := s.listener.Accept()
+			if err != nil {
+				select {
+				case <-s.quit:
+					return
+				default:
+					log.Fatal("accept error:", err)
+				}
+			}
+			s.wg.Add(1)
+			go func() {
+				s.rpcServer.ServeConn(conn)
+				s.wg.Done()
+			}()
+		}
+	}()
+}
+
+// DisconnectAll closes all the client connections to peers for this server.
+func (s *Server) DisconnectAll() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for id := range s.peerClients {
+		if s.peerClients[id] != nil {
+			s.peerClients[id].Close()
+			s.peerClients[id] = nil
+		}
+	}
+}
+
+// Shutdown closes the server and waits for it to shut down properly.
+func (s *Server) Shutdown() {
+	s.rf.Kill()
+	close(s.quit)
+	s.listener.Close()
+	s.wg.Wait()
+}
+
+func (s *Server) GetListenAddr() net.Addr {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.listener.Addr()
+}
+
+func (s *Server) ConnectToPeer(peerId int, addr net.Addr) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.peerClients[peerId] == nil {
+		client, err := rpc.Dial(addr.Network(), addr.String())
+		if err != nil {
+			return err
+		}
+		s.peerClients[peerId] = client
+	}
+	return nil
+}
+
+// DisconnectPeer disconnects this server from the peer identified by peerId.
+func (s *Server) DisconnectPeer(peerId int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.peerClients[peerId] != nil {
+		err := s.peerClients[peerId].Close()
+		s.peerClients[peerId] = nil
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Call(id int, serviceMethod string, args any, reply any) error {
+	s.mu.Lock()
+	peer := s.peerClients[id]
+	s.mu.Unlock()
+
+	// If this is called after shutdown (where client.Close is called), it will
+	// return an error.
+	if peer == nil {
+		return fmt.Errorf("call client %d after it's closed", id)
+	} else {
+		return peer.Call(serviceMethod, args, reply)
+	}
+}
+
+func (rpp *RPCProxy) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
+	if len(os.Getenv("RAFT_UNRELIABLE_RPC")) > 0 {
+		dice := rand.Intn(10)
+		if dice == 9 {
+			rpp.rf.dlog("drop RequestVote")
+			return fmt.Errorf("RPC failed")
+		} else if dice == 8 {
+			rpp.rf.dlog("delay RequestVote")
+			time.Sleep(75 * time.Millisecond)
+		}
+	} else {
+		time.Sleep(time.Duration(1+rand.Intn(5)) * time.Millisecond)
+	}
+	return rpp.rf.RequestVote(args, reply)
+}
