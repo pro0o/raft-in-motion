3ad3757b5ab846f022009ae565bff0ddef9ccb51
pro0o <pp48041721@student.ku.edu.np>
2024-10-13 16:51:40 +0545

    feat(raft): Elections, timeout & RequestVoteRPC.

--- raft/raft.go
+++ raft/raft.go
@@ -1,15 +1,13 @@
 package raft
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
 	"sync"
 	"time"
 )
-
-// as each Raft peer becomes aware that successive Log entries are
-// committed, the peer should send an ApplyMsg to the service (or
-// tester) on the same server, via the ApplyCh passed to Make(). set
-// CommandValid to true to indicate that the ApplyMsg contains a newly
-// committed Log entry.
 
 const DebugRf = 1
 
@@ -31,10 +29,10 @@ const (
 type Raft struct {
 	mu      sync.Mutex
 	id      int   // server ID.
-	peerIds []int //end points of all Peers
+	peerIds []int // end points of all Peers
 
-	// server is the server containing this CM. It's used to issue RPC calls
-	// to peers.
+	// Server is the server containing this Rf.
+	// issue RPC calls to peers.
 	server *Server
 
 	// persistant States
@@ -47,6 +45,7 @@ type Raft struct {
 	electionResetEvent time.Time
 }
 
+// key states defined straight outta figure2 of paper
 type RequestVoteArgs struct {
 	Term         int
 	CandidateId  int
@@ -87,4 +86,263 @@ func (rf RfState) String() string {
 	default:
 		panic("unreachable")
 	}
+}
+
+func (rf *Raft) Kill() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	rf.state = Dead
+	rf.dlog("becomes Dead")
+}
+
+// the service or tester wants to create a Raft server. the ports
+// of all the Raft servers (including this one) are in Peers[]. this
+// server's port is Peers[me]. all the servers' Peers[] arrays
+// have the same order. Persister is a place for this server to
+// save its persistent State, and also initially holds the most
+// recent saved State, if any. ApplyCh is a channel on which the
+// tester or service expects Raft to send ApplyMsg messages.
+// Make() must return quickly, so it should start goroutines
+// for any long-running work.
+
+func Make(id int, peerIds []int, server *Server, ready <-chan any) *Raft {
+	rf := &Raft{
+		id:       id,
+		peerIds:  peerIds,
+		server:   server,
+		state:    Follower, // all peers start as follower
+		votedFor: -1,
+	}
+
+	go func() {
+		<-ready
+		rf.mu.Lock()
+		rf.electionResetEvent = time.Now()
+		rf.mu.Unlock()
+		rf.runElectionTimer()
+	}()
+
+	return rf
+}
+
+// follower_timeout -> become_candidate -> request_votes
+// => if_majority_then_become_leader => send_heartbeats
+// => if_higher_term_then_step_down => revert_to_follower => repeat
+
+func (rf *Raft) runElectionTimer() {
+	timeoutDuration := rf.electionTimeout()
+	rf.mu.Lock()
+	termStarted := rf.currentTerm
+	rf.mu.Unlock()
+	rf.dlog("election timer started (%v), term=%d", timeoutDuration, termStarted)
+
+	// This loops until either:
+	// we discover the election timer is no longer needed, or
+	// the election timer expires and this Rf becomes a candidate
+
+	// while in a follower though, this typically keeps running in the background for the
+	// duration of the Rf's lifetime.
+	// until leader fumbles the baddie
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+	//main followers timeout
+	for {
+		<-ticker.C
+
+		rf.mu.Lock()
+		if rf.state != Candidate && rf.state != Follower {
+			rf.dlog("in election timer state=%s, bailing out", rf.state)
+			rf.mu.Unlock()
+			return
+		}
+
+		if termStarted != rf.currentTerm {
+			rf.dlog("in election timer term changed from %d to %d, bailing out", termStarted, rf.currentTerm)
+			rf.mu.Unlock()
+			return
+		}
+
+		// Start an election if we haven't heard from a leader or haven't voted for
+		// someone for the duration of the timeout.
+		if elapsed := time.Since(rf.electionResetEvent); elapsed >= timeoutDuration {
+			rf.startElection()
+			rf.mu.Unlock()
+			return
+		}
+		rf.mu.Unlock()
+	}
+}
+
+// electionTimeout generates a pseudo-random election timeout duration.
+func (rf *Raft) electionTimeout() time.Duration {
+	// If RAFT_FORCE_MORE_REELECTION is set, stress-test by deliberately
+	// generating a hard-coded number very often. This will create collisions
+	// between different servers and force more re-elections.
+	if len(os.Getenv("RAFT_FORCE_MORE_REELECTION")) > 0 && rand.Intn(3) == 0 {
+		return time.Duration(150) * time.Millisecond
+	} else {
+		return time.Duration(150+rand.Intn(150)) * time.Millisecond
+	}
+}
+
+func (rf *Raft) dlog(format string, args ...any) {
+	if DebugRf > 0 {
+		format = fmt.Sprintf("[%d] ", rf.id) + format
+		log.Printf(format, args...)
+	}
+}
+
+func (rf *Raft) startElection() {
+	rf.state = Candidate
+	rf.currentTerm += 1
+	savedCurrentTerm := rf.currentTerm
+	rf.electionResetEvent = time.Now()
+	rf.votedFor = rf.id
+	rf.dlog("becomes Candidate (currentTerm=%d); log=%v", savedCurrentTerm, rf.log)
+
+	votesReceived := 1
+
+	// Send RequestVote RPCs to all other servers concurrently.
+	for _, peerId := range rf.peerIds {
+		go func(peerId int) {
+			args := RequestVoteArgs{
+				Term:        savedCurrentTerm,
+				CandidateId: rf.id,
+			}
+			var reply RequestVoteReply
+
+			rf.dlog("sending RequestVote to %d: %+v", peerId, args)
+			if err := rf.server.Call(peerId, "Raft.RequestVote", args, &reply); err == nil {
+				rf.mu.Lock()
+				defer rf.mu.Unlock()
+				rf.dlog("received RequestVoteReply %+v", reply)
+
+				if rf.state != Candidate {
+					rf.dlog("while waiting for reply, state = %v", rf.state)
+					return
+				}
+
+				if reply.Term > savedCurrentTerm {
+					rf.dlog("term out of date in RequestVoteReply")
+					rf.becomeFollower(reply.Term)
+					return
+				} else if reply.Term == savedCurrentTerm {
+					if reply.VoteGranted {
+						votesReceived += 1
+						if votesReceived*2 > len(rf.peerIds)+1 {
+							// Won the election!!!
+							rf.dlog("wins election with %d votes", votesReceived)
+							rf.startLeader()
+							return
+						}
+					}
+				}
+			}
+		}(peerId)
+	}
+
+	// Run another election timer, in case this election is not successful.
+	go rf.runElectionTimer()
+}
+
+// after eventual loss of leadership/ candidate,
+// run election
+func (rf *Raft) becomeFollower(term int) {
+	rf.dlog("becomes Follower with term=%d; log=%v", term, rf.log)
+	rf.state = Follower
+	rf.currentTerm = term
+	rf.votedFor = -1
+	rf.electionResetEvent = time.Now()
+
+	go rf.runElectionTimer()
+}
+
+// startLeader switches rf into a leader state and begins process of heartbeats.
+// heartbeats as in the AE
+func (rf *Raft) startLeader() {
+	rf.state = Leader
+	rf.dlog("becomes Leader; term=%d, log=%v", rf.currentTerm, rf.log)
+
+	go func() {
+		ticker := time.NewTicker(50 * time.Millisecond)
+		defer ticker.Stop()
+
+		// Send periodic heartbeats, as long as still leader.
+		for {
+			rf.leaderSendHeartbeats()
+			<-ticker.C
+
+			rf.mu.Lock()
+			if rf.state != Leader {
+				rf.mu.Unlock()
+				return
+			}
+			rf.mu.Unlock()
+		}
+	}()
+}
+
+// leaderSendHeartbeats sends a round of heartbeats to all peers, collects their
+// replies and adjusts rf's state.
+func (rf *Raft) leaderSendHeartbeats() {
+	rf.mu.Lock()
+	if rf.state != Leader {
+		rf.mu.Unlock()
+		return
+	}
+	savedCurrentTerm := rf.currentTerm
+	rf.mu.Unlock()
+
+	for _, peerId := range rf.peerIds {
+		args := AppendEntriesArgs{
+			Term:     savedCurrentTerm,
+			LeaderId: rf.id,
+		}
+		go func(peerId int) {
+			rf.dlog("sending AppendEntries to %v: ni=%d, args=%+v", peerId, 0, args)
+			var reply AppendEntriesReply
+			if err := rf.server.Call(peerId, "Raft.AppendEntries", args, &reply); err == nil {
+				rf.mu.Lock()
+				defer rf.mu.Unlock()
+
+				// diffuse the leader
+				if reply.Term > savedCurrentTerm {
+					rf.dlog("term out of date in heartbeat reply")
+					rf.becomeFollower(reply.Term)
+					return
+				}
+			}
+		}(peerId)
+	}
+}
+
+// RequestVote RPC from figure 2 of paper
+func (rf *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if rf.state == Dead {
+		return nil
+	}
+	rf.dlog("RequestVote: %+v [currentTerm=%d, votedFor=%d]", args, rf.currentTerm, rf.votedFor)
+
+	// from the figure 2 itself
+	// 1. Reply false if term < currentTerm
+	if args.Term > rf.currentTerm {
+		rf.dlog("... term out of date in RequestVote")
+		rf.becomeFollower(args.Term)
+	}
+
+	// 2. If VotedFor is null or CandidateId, and candidate’s Log is at
+	// least as up-to-date as receiver’s log, grant vote
+	if rf.currentTerm == args.Term &&
+		(rf.votedFor == -1 || rf.votedFor == args.CandidateId) {
+		reply.VoteGranted = true
+		rf.votedFor = args.CandidateId
+		rf.electionResetEvent = time.Now()
+	} else {
+		reply.VoteGranted = false
+	}
+	reply.Term = rf.currentTerm
+	rf.dlog("... RequestVote reply: %+v", reply)
+	return nil
 }
