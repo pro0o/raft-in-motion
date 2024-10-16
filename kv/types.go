55584c61a5a7bebe0c44159e6a753b0fd1dff15b
pro0o <pp48041721@student.ku.edu.np>
2024-10-16 16:44:09 +0545

    feat(kv): A very simple get & put key-store; types def for now.

---
+++ kv/types.go
@@ -0,0 +1,57 @@
+package kv
+
+// put
+type PutRequest struct {
+	Key   string
+	Value string
+}
+
+type Response interface {
+	Status() ResponseStatus
+}
+
+type PutResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	PrevValue  string
+}
+
+func (pr *PutResponse) Status() ResponseStatus {
+	return pr.RespStatus
+}
+
+// get
+type GetRequest struct {
+	Key string
+}
+
+type GetResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	Value      string
+}
+
+func (gr *GetResponse) Status() ResponseStatus {
+	return gr.RespStatus
+}
+
+// application layer(kv store) -> raft
+type ResponseStatus int
+
+const (
+	StatusInvalid ResponseStatus = iota
+	StatusOK
+	StatusNotLeader
+	StatusFailedCommit
+)
+
+var responseName = map[ResponseStatus]string{
+	StatusInvalid:      "invalid",
+	StatusOK:           "OK",
+	StatusNotLeader:    "NotLeader",
+	StatusFailedCommit: "FailedCommit",
+}
+
+func (rs ResponseStatus) String() string {
+	return responseName[rs]
+}
