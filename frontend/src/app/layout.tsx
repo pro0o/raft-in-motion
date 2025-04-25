4399ea89ecd858e84e3ee1baa67b8388c54b9b10
pro0o <pp48041721@student.ku.edu.np>
2025-04-26 02:31:51 +0545

    chore: meta shiiii.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,39 @@
 import './globals.css'
 import { ReactNode } from 'react'
 import { GeistMono } from 'geist/font/mono'
+import type { Metadata } from 'next'
 
-export const metadata = {
-  title: 'raft-in-motion',
-  description: 'A Raft logs demo',
+export const metadata: Metadata = {
+  metadataBase: new URL("https://raft-in-motion.vercel.app"),
+  title: {
+    default: "raft-in-motion",
+    template: "%s | raft-in-motion",
+  },
+  description: "Simulate core feats of raft.",
   icons: {
-    icon: '/favicon.ico',
-  }
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "raft-in-motion",
+    description: "A Raft consensus visualizer made using Go and Next.js.",
+    url: "https://raft-in-motion.vercel.app",
+    siteName: "raft-in-motion",
+    locale: "en_US",
+    type: "website",
+    images: ["https://raft-in-motion.vercel.app/og/home"], 
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-video-preview": -1,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+  },
+  twitter: {
+    title: "raft-in-motion",
+    card: "summary_large_image",
+    creator: "@probin",
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
